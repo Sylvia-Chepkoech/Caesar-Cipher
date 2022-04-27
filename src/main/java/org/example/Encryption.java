d3bbdbf201836627a515e1d79ead2d3bd2b784dd
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 13:51:14 +0300

    add method to encrypt users message

--- src/main/java/org/example/Encryption.java
+++ src/main/java/org/example/Encryption.java
@@ -24,5 +24,26 @@ public class Encryption {
     public void setmUserKey(int mUserKey) {
         this.mUserKey = mUserKey;
     }
+    public String encryptMessage() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder encryptedMessage = new StringBuilder();
+        String userMessage = getmUserMessage().toUpperCase();
+        char[] userMessageArray = userMessage.toCharArray();
+        int userKey = getmUserKey();
+
+        for (char b:userMessageArray) {
+            if (Character.isLetter(b)) {
+                int characterPosition = alphabet.indexOf(b);
+                int newCharacterPosition = (characterPosition + userKey) % 26;
+                char encryptedLetter = alphabet.charAt(newCharacterPosition);
+                encryptedMessage.append(encryptedLetter);
+            }else {
+                encryptedMessage.append(b);
+            }
+        }
+
+        return encryptedMessage.toString();
+    }
+
 
 }
