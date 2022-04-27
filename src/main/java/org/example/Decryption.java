0f826d0391a2fc923bc60864e33a1eab1450a53d
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 13:55:26 +0300

    add constructors of the variables

--- src/main/java/org/example/Decryption.java
+++ src/main/java/org/example/Decryption.java
@@ -5,6 +5,11 @@ public class Decryption {
     private String mUserMessage;
     private int mUserKey;
 
+    public Decryption(String userMessage, int userKey) {
+        this.mUserMessage = userMessage;
+        this.mUserKey = userKey;
+    }
+
 
 }
 
