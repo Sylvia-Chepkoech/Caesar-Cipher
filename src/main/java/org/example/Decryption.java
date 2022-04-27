82d40961079eb8eed3eb8550f6f84393b22ad70f
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 13:57:19 +0300

    add decryption method

--- src/main/java/org/example/Decryption.java
+++ src/main/java/org/example/Decryption.java
@@ -24,6 +24,36 @@ public class Decryption {
     public void setmUserKey(int mUserKey) {
         this.mUserKey = mUserKey;
     }
+    public String decryptMessage() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder decryptedMessage = new StringBuilder();
+        String userMessage = getmUserMessage().toUpperCase();
+        char[] userMessageArray = userMessage.toCharArray();
+        int userKey = getmUserKey();
+
+        for (char b:userMessageArray) {
+            if (Character.isLetter(b)) {
+                int characterPosition = alphabet.indexOf(b);
+
+                if (characterPosition != -1){
+                    int newCharacterPosition = (characterPosition - userKey) % 26;
+                    char encryptedLetter = alphabet.charAt(newCharacterPosition);
+                    decryptedMessage.append(encryptedLetter);
+                }else {
+                    int newCharacterPosition = (characterPosition - userKey) + 26;
+                    char encryptedLetter = alphabet.charAt(newCharacterPosition);
+                    decryptedMessage.append(encryptedLetter);
+                }
+
+
+            }else {
+                decryptedMessage.append(b);
+            }
+        }
+
+        return decryptedMessage.toString();
+    }
+
 
 }
 
