64c8db18aeca9d563c01dc31978905fc26c1d65c
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 13:56:39 +0300

    add getter and setter

--- src/main/java/org/example/Decryption.java
+++ src/main/java/org/example/Decryption.java
@@ -9,7 +9,21 @@ public class Decryption {
         this.mUserMessage = userMessage;
         this.mUserKey = userKey;
     }
+    public String getmUserMessage() {
+        return mUserMessage;
+    }
 
+    public void setmUserMessaage(String mUserMessaage) {
+        this.mUserMessage = mUserMessaage;
+    }
+
+    public int getmUserKey() {
+        return mUserKey;
+    }
+
+    public void setmUserKey(int mUserKey) {
+        this.mUserKey = mUserKey;
+    }
 
 }
 
