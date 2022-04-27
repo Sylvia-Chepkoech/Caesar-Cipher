3863644ed3ac0bcec0688cd85ab2cbd8368adf0f
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 14:16:36 +0300

    fix: modified decryption message

--- src/test/java/org/example/DecryptionTest.java
+++ src/test/java/org/example/DecryptionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecryptionTest {
-    Decryption newExample = new Decryption("JK", 2);
+    Decryption newExample = new Decryption("VJKU KU WU", 2);
 
     @Test
     public void decryptionMessage_returnTHIS_IS_US() {
