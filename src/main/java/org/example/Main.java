5f2d70a519480d4df6385ad7cda0ab5140b32ba9
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 14:08:08 +0300

    add a condition to be run when user chooses encryption option

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,19 @@ public class Main {
         while (programRunning){
             System.out.println("Please choose between encryption or decryption or exit");
             String userMessage = scanner.nextLine();
+            if (userMessage.equals("encryption")){
+                System.out.println("Enter word to be encrypted");
+                String messageToBeEncrypted = scanner.nextLine();
+                System.out.println("Please enter the a number between 1 to 25");
+                int userKey = Integer.parseInt(scanner.nextLine());
 
+
+                Encryption message = new Encryption(messageToBeEncrypted, userKey);
+                message.setmUserMessaage(messageToBeEncrypted);
+                message.setmUserKey(userKey);
+                System.out.println("Your encrypted message is " + message.encryptMessage());
+
+            }
         }
 
     }
