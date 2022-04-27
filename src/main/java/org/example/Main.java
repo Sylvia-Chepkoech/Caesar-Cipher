1e5f96a24505e4c4c191c25665e7768729f2a2c0
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-27 14:01:54 +0300

    add intro statement to user

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Boolean programRunning = true;
+
+        System.out.println("Hello world!Welcome To Caeser Cipher");
+
+
+    }
+}
